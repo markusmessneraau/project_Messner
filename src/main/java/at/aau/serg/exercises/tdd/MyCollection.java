85931197fec615d4e02a5c4e395683052e3786c9
Markus Messner <markusmes@edu.aau.at>
2022-12-12 21:37:46 +0100

    Testfälle erstellt um zu überprüfen ob ein Element enfernt worden ist und ob eine Exeption geworfen wird

--- src/main/java/at/aau/serg/exercises/tdd/MyCollection.java
+++ src/main/java/at/aau/serg/exercises/tdd/MyCollection.java
@@ -41,4 +41,9 @@ public class MyCollection {
 
     }
 
+    public String getElement (int x){
+
+        return list[x];
+    }
+
 }
--- src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
+++ src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
@@ -1,13 +1,64 @@
 package at.aau.serg.exercises.tdd;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitPlatform.class)
+public class MyCollectionTest {
 
-public class MyCollectionTest
-{
+    @Test
+    public void testRemove(){
+
+        MyCollection x = new MyCollection(4);
+
+        ArrayList<String> a = new ArrayList<>();
+
+
+
+        x.add("VW");
+        x.add("BMX");
+        x.add("Audi");
+        x.add("Fiat");
+
+        x.remove("VW");
+
+
+        for (int  i = 0;  i < x.size()-1;  i++) {
+
+            a.add(x.getElement(i));
+
+        }
+
+        Assertions.assertFalse(a.contains("VW"));
+
+
+
+    }
+
+
+
+
+
+    @Test
+    public void testRemoveExeption(){
+
+        MyCollection x = new MyCollection(4);
+
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+
+
+            x.remove("Dell");
+
+        });
+    }
 
 }
